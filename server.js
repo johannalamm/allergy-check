2ce4a3d94081fbe01b471d40a00c05897310df71
Johanna Thorsen <johanna.thorsen@live.se>
2020-06-29 23:49:35 +0200

    feat: add express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port}.`));
