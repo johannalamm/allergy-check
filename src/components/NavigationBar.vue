7b6bb713b0347e653d58be1051d4420ff3dc17c6
Johanna Thorsen <johanna.thorsen@live.se>
2020-09-19 00:33:26 +0200

    setup basic vue app

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="center examplex">
+    <vs-navbar center-collapsed v-model="active">
+      <template #left>
+        <img src="/logo2.png" alt="">
+      </template>
+      <vs-navbar-item :active="active == 'guide'" id="guide">
+      <router-link to="/">Home</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'docs'" id="docs">
+      <router-link to="/about">About</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'components'" id="components">
+        Components
+      </vs-navbar-item>
+      <vs-navbar-item :active="active == 'license'" id="license">
+        license
+      </vs-navbar-item>
+      <template #right>
+        <vs-button flat >Login</vs-button>
+        <vs-button>Get Started</vs-button>
+      </template>
+    </vs-navbar>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    active: 'guide'
+  })
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
